a8441ca6c3fc7d30a1eee66db378db9c8f5db44d
miguelsabino23 <miguelsabino.office@gmail.com>
2024-06-06 23:09:15 -0300

    feat: adicionar estilo a seção testimonial

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,13 @@
 const swiper = new Swiper('.swiper', {
-    
     slidesPerView: 4,
     spaceBetween: 10,
     loop: true,
 
-    
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
 
-   
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
@@ -20,6 +17,25 @@ const swiper = new Swiper('.swiper', {
         delay: 5000,
         pauseOnMouseEnter: true,
     },
+})
 
-    
+const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 2,
+    spaceBetween: 20,
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination2',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next2',
+        prevEl: '.swiper-button-prev2',
+    },
+    autoplay: {
+        delay: 5000,
+        pauseOnMouseEnter: true,
+    },
+    slidesPerGroup: 2,
 })
