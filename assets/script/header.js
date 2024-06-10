9105f33940a5e74fb51a1dc9ca94323c9d7df25b
miguelsabino23 <miguelsabino.office@gmail.com>
2024-06-09 21:24:42 -0300

    feat: adicionar funcionalidade ao header

--- assets/script/header.js
+++ assets/script/header.js
@@ -2,7 +2,6 @@ const header = document.getElementById('header')
 const home = document.getElementById('home')
 document.addEventListener('scroll', () => {
     const posicaoY = window.pageYOffset
-    header.style.backgroundColor = posicaoY == 0 ? 'transparent' : '#000000'
     if (posicaoY >= 100) {
         header.style.backgroundColor = '#000000'
         header.style.position = 'fixed'
