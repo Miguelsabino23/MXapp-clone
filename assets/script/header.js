db9a600ee69b7cf9f6ae1790e5f5d4d84629ff6c
miguelsabino23 <miguelsabino.office@gmail.com>
2024-06-11 19:41:00 -0300

    fix: ultimos detalhes

--- assets/script/header.js
+++ assets/script/header.js
@@ -10,3 +10,28 @@ document.addEventListener('scroll', () => {
         header.style.backgroundColor = 'transparent'
     }
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section')
+    const navLinks = document.querySelectorAll('.nav__link')
+
+    window.addEventListener('scroll', function () {
+        let current = 'headerr'
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute('id')
+            }
+        })
+
+        navLinks.forEach((link) => {
+            link.classList.remove('active')
+            if (link.getAttribute('href').includes(current)) {
+                link.classList.add('active')
+            }
+        })
+    })
+})
